703a1107969da30a280ed598d9516c1f7039a329
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-03-30 10:50:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Inigo Montoya"
+  end
+
+  get "/hometown" do
+    "My hometown is ... You killed my father"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Prepare to die.'"
+  end
 end
