33c20255754c4387817ac8aecc7110ec8131f5e7
JackyLin2 <jacky.jia.lin@gmail.com>
2020-03-30 01:07:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Jacky"
+    end 
+ 
+    get '/hometown' do 
+        "My hometown is Brooklyn"
+    end 
+    
+    get '/favorite-song' do 
+        "My favorite song is Blinding Lights"
+    end 
+
+
 end
